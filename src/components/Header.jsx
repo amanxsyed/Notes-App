08436dc975dd03f8730b718a608e73a8ca44118d
Syed <amanxsyed@gmail.com>
2025-02-08 03:59:54 +0500

    add Note components, implement tagging functionality, and update package dependencies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({ handleToggleDarkMode, darkMode }) => {
       <div className="container mx-auto flex items-center justify-between">
         <h1 className="text-5xl font-bold text-green-900">Notes</h1>
         <button
-          className="toggle my-6 p-2 text-xl rounded-full bg-green-600 hover:bg-green-900 transition duration-500 text-green-50"
+          className=" my-6 p-2 text-xl rounded-full bg-green-600 hover:bg-green-900 transition duration-500 text-green-50"
           onClick={() => handleToggleDarkMode((prev) => !prev)} 
         >
           {darkMode ? <FaSun /> : <FaMoon />}
